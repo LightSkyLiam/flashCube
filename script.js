e2e220474f2c258f8664f653b555e3f364e20da3
LightSkyLiam <liamlasry2000@gmail.com>
2023-06-17 17:40:32 +0300

    added foncunalities, event listener bug still exists

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
 `use strict`;
 const startGame = document.getElementById(`start`);
 const cubeList = document.querySelector(`.cubes`);
+const displayedScore = document.querySelector(`.score`);
+const displayedHighScore = document.querySelector(`.high-score`);
 
-let activeCube = Math.trunc(Math.random() * 9 + 1);
-let cubeInterval;
-let clickedCube = 0;
-function delay(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-const checkCube = (evt) => {
-  let clickedCube = evt.target;
-  console.log(clickedCube.textContent);
+let clickedCube,
+  currentScore = 0,
+  highScore = 0,
+  activeCube,
+  didUserClicked,
+  cubeInterval;
+
+// check which cube click and if it's correct cube (if not, stops the interval);
+const checkCube = (evt, Interval) => {
+  clickedCube = Number(evt.target.textContent);
+  if (clickedCube !== activeCube) {
+    lostGame(Interval);
+  } else {
+    didUserClicked = true;
+    currentScore++;
+    displayedScore.textContent = `ניקוד: ${currentScore}`;
+  }
 };
+//resets the game
+const lostGame = (Interval) => {
+  clearInterval(Interval);
+  if (currentScore > highScore) {
+    highScore = currentScore;
+    displayedHighScore.textContent = `שיא: ${highScore}`;
+  }
+  currentScore = 0;
+  displayedScore.textContent = `ניקוד: ${currentScore}`;
+  document.getElementById(`cube--${activeCube}`).classList.remove(`active`);
+  activeCube = 0;
+  startGame.removeEventListener(`click`);
+};
+
 const check = startGame.addEventListener(`click`, () => {
   const Interval = setInterval(() => {
-    cubeList.addEventListener(`click`, () => {
-      checkCube;
-    });
-    let lastCube = activeCube;
-    document.getElementById(`cube--${activeCube}`).classList.add(`active`);
+    //generate a random numb and display the cube as active
     activeCube = Math.trunc(Math.random() * 9 + 1);
-    delay(500).then(() =>
-      document.getElementById(`cube--${lastCube}`).classList.remove(`active`)
-    );
-    cubeList.addEventListener(`click`, checkCube);
-    // if (clickedCube !== activeCube) {
-    //   clearInterval(Interval);
-    // }
-  }, 700);
+    didUserClicked = false;
+    document.getElementById(`cube--${activeCube}`).classList.add(`active`);
+    //waits 1 second then remove the active cube and checks if a user clicked
+    setTimeout(() => {
+      document.getElementById(`cube--${activeCube}`).classList.remove(`active`);
+      if (!didUserClicked) {
+        lostGame(Interval);
+      }
+    }, 2000);
+  }, 2200);
+
+  // on click - calls the checkCube to check if it was correct cube - if not - stops the game
+  cubeList.addEventListener(`click`, (eventClick) => {
+    checkCube(eventClick, Interval);
+  });
 });
