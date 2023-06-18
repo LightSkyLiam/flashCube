fc351767c0a85659fcb166db34263ae47b2a0b97
LightSkyLiam <liamlasry2000@gmail.com>
2023-06-18 10:37:09 +0300

    added board sizes functions

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const cubeList = document.querySelector(`.cubes`);
 const displayedScore = document.querySelector(`.score`);
 const displayedHighScore = document.querySelector(`.high-score`);
 const resetButton = document.getElementById(`reset`);
+const cubesUl = document.querySelector(`.cubes`);
 const difficultyButtons = [
   document.getElementById(`easy`),
   document.getElementById(`medium`),
   document.getElementById(`extreme`),
+];
+const boardSizeButtons = [
+  document.getElementById(`3x3`),
+  document.getElementById(`4x4`),
+  document.getElementById(`5x5`),
 ];
 
 let clickedCube,
@@ -18,20 +24,77 @@ let clickedCube,
   cubeInterval,
   startClicks = 0,
   difficultyMode = `easy`,
-  speedOfCubes = 2000;
+  speedOfCubes = 2000,
+  numOfCubes = 9;
 displayedHighScore.textContent = `שיא: ${highScore}`;
 
-// check which cube click and if it's correct cube (if not, stops the interval);
-const checkCube = (evt, Interval) => {
-  clickedCube = Number(evt.target.textContent);
-  if (clickedCube !== activeCube) {
-    init(Interval);
-  } else if (!didUserClicked) {
-    didUserClicked = true;
-    currentScore++;
-    displayedScore.textContent = `ניקוד: ${currentScore}`;
+for (let i = 1; i <= 9; i++) {
+  const tempCube = cubesUl.appendChild(document.createElement("li"));
+  tempCube.classList.add(`cube`);
+  tempCube.setAttribute(`id`, `cube--${i}`);
+  tempCube.textContent = String(i);
+}
+
+const switchBoardSize = (boardSize) => {
+  switch (boardSize.target.id) {
+    case `3x3`:
+      if (!cubesUl.classList.contains(`grid-3-cols`)) {
+        cubesUl.innerHTML = ``;
+        numOfCubes = 9;
+        cubesUl.classList.add(`grid-3-cols`);
+        cubesUl.classList.remove(`grid-5-cols`);
+        cubesUl.classList.remove(`grid-4-cols`);
+        for (let i = 1; i <= 9; i++) {
+          const tempCube = cubesUl.appendChild(document.createElement("li"));
+          tempCube.classList.add(`cube`);
+          tempCube.setAttribute(`id`, `cube--${i}`);
+          tempCube.textContent = String(i);
+        }
+        boardSizeButtons[0].classList.remove(`secondary-btn`);
+        boardSizeButtons[1].classList.add(`secondary-btn`);
+        boardSizeButtons[2].classList.add(`secondary-btn`);
+        break;
+      }
+      break;
+    case `4x4`:
+      if (!cubesUl.classList.contains(`grid-4-cols`)) {
+        cubesUl.innerHTML = ``;
+        numOfCubes = 16;
+        cubesUl.classList.remove(`grid-3-cols`);
+        cubesUl.classList.remove(`grid-5-cols`);
+        cubesUl.classList.add(`grid-4-cols`);
+        for (let i = 1; i <= 16; i++) {
+          const tempCube = cubesUl.appendChild(document.createElement("li"));
+          tempCube.classList.add(`cube`);
+          tempCube.setAttribute(`id`, `cube--${i}`);
+          tempCube.textContent = String(i);
+        }
+        boardSizeButtons[1].classList.remove(`secondary-btn`);
+        boardSizeButtons[0].classList.add(`secondary-btn`);
+        boardSizeButtons[2].classList.add(`secondary-btn`);
+        break;
+      }
+    case `5x5`:
+      if (!cubesUl.classList.contains(`grid-5-cols`)) {
+        cubesUl.innerHTML = ``;
+        numOfCubes = 25;
+        cubesUl.classList.remove(`grid-3-cols`);
+        cubesUl.classList.add(`grid-5-cols`);
+        cubesUl.classList.remove(`grid-4-cols`);
+        for (let i = 1; i <= 25; i++) {
+          const tempCube = cubesUl.appendChild(document.createElement("li"));
+          tempCube.classList.add(`cube`);
+          tempCube.setAttribute(`id`, `cube--${i}`);
+          tempCube.textContent = String(i);
+        }
+        boardSizeButtons[2].classList.remove(`secondary-btn`);
+        boardSizeButtons[1].classList.add(`secondary-btn`);
+        boardSizeButtons[0].classList.add(`secondary-btn`);
+        break;
+      }
   }
 };
+
 //resets the game
 const init = (Interval) => {
   clearInterval(Interval);
@@ -86,7 +149,7 @@ const gamePlay = () => {
   if (startClicks <= 1) {
     const Interval = setInterval(() => {
       //generate a random numb and display the cube as active
-      activeCube = Math.trunc(Math.random() * 9 + 1);
+      activeCube = Math.trunc(Math.random() * numOfCubes + 1);
       didUserClicked = false;
       document.getElementById(`cube--${activeCube}`).classList.add(`active`);
       //waits 1 second then remove the active cube and checks if a user clicked
@@ -105,10 +168,27 @@ const gamePlay = () => {
     });
   }
 };
+
+// check which cube click and if it's correct cube (if not, stops the interval);
+const checkCube = (evt, Interval) => {
+  clickedCube = Number(evt.target.textContent);
+  if (clickedCube !== activeCube) {
+    init(Interval);
+  } else if (!didUserClicked) {
+    didUserClicked = true;
+    currentScore++;
+    displayedScore.textContent = `ניקוד: ${currentScore}`;
+  }
+};
 startGame.addEventListener(`click`, gamePlay);
 resetButton.addEventListener(`click`, resetGame);
 difficultyButtons.forEach((item) => {
   item.addEventListener(`click`, (evt) => {
     switchDifficulty(evt);
+  });
+});
+boardSizeButtons.forEach((item) => {
+  item.addEventListener(`click`, (evt) => {
+    switchBoardSize(evt);
   });
 });
